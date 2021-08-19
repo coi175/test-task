42fb501ed7db4dd125e36356596ba67dae79b5a4
Евгений Фабер <coikarp175@gmail.com>
2021-08-19 13:14:19 +0600

    Added files

--- src/main/java/ProducerConsumerService.java
+++ src/main/java/ProducerConsumerService.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-public class ProcucerConsumerService
+public class ProducerConsumerService
 {
     private final List<Integer> randoms = new ArrayList<>();
-    
+    private static final int RANDOM_NUMBER_RANGE = 100;
+    private static final int RANDOM_NUMBERS_COUNT = 100;
+    private static final int THREADS_SLEEP_TIME = 1000;
     public void startThreads()
     {
         producer.start();
@@ -13,15 +16,47 @@ public class ProcucerConsumerService
 
     private final Thread producer = new Thread(() ->
     {
-        synchronized (randoms) {
+        while (true)
+        {
+            Integer numsCount = new Random().nextInt(RANDOM_NUMBERS_COUNT);
+            synchronized (randoms) {
+                for (int i = 0; i < numsCount; i++)
+                {
+                    Integer number = new Random().nextInt(RANDOM_NUMBER_RANGE);
+                    randoms.add(number);
+                    System.out.print(number + " ");
+                }
+                System.out.println("\n Wrote by producer");
+            }
 
+            try
+            {
+                Thread.sleep(THREADS_SLEEP_TIME);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
         }
     });
 
     private final Thread consumer = new Thread(() ->
     {
-        synchronized (randoms) {
+        while (true)
+        {
+            synchronized (randoms) {
+                randoms.forEach(x -> System.out.print(x + " "));
+                randoms.clear();
+                System.out.println("\n Read by consumer");
+            }
 
+            try
+            {
+                Thread.sleep(THREADS_SLEEP_TIME);
+            }
+            catch (InterruptedException e) {
+
+            }
         }
     });
 }
